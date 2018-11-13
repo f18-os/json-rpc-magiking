6dcf243ab37fcb66e782b3238194f15f14dca3bc
mk <magiking@magiking.org>
2018-11-12 20:46:34 -0700

    Add encode and decode to node

--- graph/node.py
+++ graph/node.py
@@ -13,3 +13,40 @@ def increment(graph):
     for c in graph.children:
         increment(c)
 
+def encodeGraph(root, nodes={}):
+    print("encoding a graph...")
+    # nodes
+    if id(root) not in nodes:
+        newNode = {}
+        newNode['name'] = root.name
+        newNode['val'] =  root.val
+        nodes[id(root)] = newNode
+    # graph structure
+    encoded = {}
+    encoded['id'] = id(root)
+    encoded['children'] = []
+    # children
+    for child in root.children:
+        childGraph, nodesCopy = encodeGraph(child, nodes)
+        encoded['children'].append(childGraph)
+    return encoded, nodes
+
+def decodeGraph(graph, nodes):
+    print("decoding a graph...")
+    id_ = graph['id']
+    if str(id_) in nodes:
+        id_ = str(id_)
+        print("using string version of 'id': ", id_)
+    info = nodes[id_] # get the info for that node
+    print("info: ", info)
+    root = node(info['name'])
+    root.val = info['val']
+    root.children = []
+    for child in graph['children']:
+        root.children.append(decodeGraph(child, nodes))
+    return root
+    
+def show_id(graph):
+    print(graph.name, ": ", id(graph))
+    for c in graph.children:
+        show_id(c)
